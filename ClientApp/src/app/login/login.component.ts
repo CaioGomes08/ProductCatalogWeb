449a2d2997dc83f2d80714be992d0470a5f4e486
Caio Gomes <caiovini-2015@bol.com.br>
2019-11-21 23:29:51 -0300

    metodo de login

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/model/user.model';
+import { AuthenticationService } from 'src/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,10 @@ import { User } from 'src/model/user.model';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService,
+              private router: Router) { }
+
+  invalidLogin: boolean;
 
   ngOnInit() {
   }
@@ -17,6 +22,18 @@ export class LoginComponent implements OnInit {
   user: User = new User();
 
   logar() {
-    console.log(this.user);
+    this.authService.login(this.user)
+        .subscribe(response => {
+          const token = response['token'];
+          localStorage.setItem('token', token);
+          this.invalidLogin = false;
+          this.router.navigate(['/home']);
+        }, err => {
+          this.invalidLogin = true;
+        });
+  }
+
+  logout(){
+    localStorage.removeItem('token');
   }
 }
