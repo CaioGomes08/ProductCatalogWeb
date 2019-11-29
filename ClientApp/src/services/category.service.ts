ba15a35100d98f554b49b1540f8374f9adfe0e60
Caio Gomes <caiovini-2015@bol.com.br>
2019-11-28 21:58:22 -0300

    ajustes no service

--- ClientApp/src/services/category.service.ts
+++ ClientApp/src/services/category.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Category } from '../model/category.model';
 import { ResultViewModel } from '../model/resultViewModel.model';
-import {  HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
@@ -22,47 +22,22 @@ export class CategoryService {
   });
 
   public getCategories() {
-    return this.httpClient.get<Category[]>(`${environment.storeApi}/category`, {headers: this.headers})
-      .pipe(
-        map(response => {
-          return response;
-        })
-      );
+    return this.httpClient.get<Category[]>(`${environment.storeApi}/category`, { headers: this.headers });
   }
 
   public getCategoryById(id: number) {
-    return this.httpClient.get<Category>(`${environment.storeApi}/category/${id}`, {headers: this.headers})
-      .pipe(
-        map(response => {
-          return response;
-        })
-      );
+    return this.httpClient.get<Category>(`${environment.storeApi}/category/${id}`, { headers: this.headers });
   }
 
   public createCategory(category: Category) {
-    return this.httpClient.post<ResultViewModel>(`${environment.storeApi}/category`, category , {headers: this.headers})
-      .pipe(
-        map(response => {
-          return response;
-        })
-      );
+    return this.httpClient.post<ResultViewModel>(`${environment.storeApi}/category`, category, { headers: this.headers });
   }
 
   public updateCategory(category: Category) {
-    return this.httpClient.put<ResultViewModel>(`${environment.storeApi}/category`, category , {headers: this.headers})
-      .pipe(
-        map(response => {
-          return response;
-        })
-      );
+    return this.httpClient.put<ResultViewModel>(`${environment.storeApi}/category`, category, { headers: this.headers });
   }
 
   public deleteCategory(id: number) {
-    return this.httpClient.delete<ResultViewModel>(`${environment.storeApi}/category/${id}` , {headers: this.headers})
-      .pipe(
-        map(response => {
-          return response;
-        })
-      );
+    return this.httpClient.delete<ResultViewModel>(`${environment.storeApi}/category/${id}`, { headers: this.headers });
   }
 }
