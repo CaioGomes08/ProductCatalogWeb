eec1d0593b96266dbf12bf8b0337424bc349eb03
Caio Gomes <caiovini-2015@bol.com.br>
2020-03-30 09:17:26 -0300

    Remove protractor

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { browser } from 'protractor';
 import { Router } from '@angular/router';
 import { AuthGuard } from '../guards/auth-guard.service';
 import { User } from 'src/model/user.model';
