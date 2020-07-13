1e8894a5b2d60fe76b01fcdfca8c693201dc7a2d
Caio Gomes <caiovini-2015@bol.com.br>
2020-07-13 18:47:35 -0300

    Auth guard

--- ClientApp/src/app/guards/auth-guard.service.ts
+++ ClientApp/src/app/guards/auth-guard.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, EventEmitter } from "@angular/core";
-import { CanActivate, Router } from "@angular/router";
-import { JwtHelperService } from "@auth0/angular-jwt";
+import { Injectable, EventEmitter } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
